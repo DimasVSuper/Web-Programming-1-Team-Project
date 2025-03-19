a01cdebbf6917ca24e80101c5c4869ef66421c90
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-03-19 15:18:44 +0700

    Adding Bootstrap to NPM and stop using CDN

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // filepath: c:\Users\Dimas\Documents\projectwebpro1\src\main.js
 import './style.css';
 import { home } from './home.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 // Render halaman utama langsung
 document.querySelector('#app').innerHTML = home();
