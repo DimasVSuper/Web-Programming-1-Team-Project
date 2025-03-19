f548af0b891eb2f3a2b8d968718df531ffbbb12b
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-03-19 15:20:22 +0700

    Deleting some files

--- src/about.js
+++
@@ -1,8 +0,0 @@
-export function about() {
-    return `
-        <div class="content">
-            <h2>About Page</h2>
-            <p>Ini adalah halaman About. Kami menyediakan layanan terbaik untuk Anda.</p>
-        </div>
-    `;
-}
